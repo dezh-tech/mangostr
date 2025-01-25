57f06741bed794d085f56a10bb17b07ca19b6ef2
Kay <kehiiiiya@gmail.com>
2025-01-25 19:29:22 +0000

    fix: add dist to ignore.

--- dist/postcss.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {}
-    }
-};
-//# sourceMappingURL=postcss.config.js.map
