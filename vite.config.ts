9ca431498ba556303fbc0def0ad002a886e71599
Kay <kehiiiiya@gmail.com>
2025-06-12 08:13:36 +0000

    Stack: nostr:naddr1qvzqqqrhl5pzqprpljlvcnpnw3pejvkkhrc3y6wvmd7vjuad0fg2ud3dky66gaxaqqrk66mnw3skx6c4g6ltw

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,28 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import path from "node:path";
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
+
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    onConsoleLog(log) {
+      return !log.includes("React Router Future Flag Warning");
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
